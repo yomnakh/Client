b0f43801ef73e6da9664fab7cf1287b360967c5c
yomnaa3 <132593493+yomnaa3@users.noreply.github.com>
2024-05-05 18:30:59 +0300

    learning component

---
+++ src/components/Mylearning/Learning.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./learning.css";
+
+
+const Learning = () => {
+  return (
+    <div className="col">
+    <div className="mylearning-header">
+      <div className="d-flex justify-content-center align-content-center">
+        <h2 className="header-mylearning">My Courses</h2>
+      </div>
+     
+    </div>
+  </div>
+  )
+}
+
+export default Learning
