27f1ea2ebab4ce8446fe2f1143999a59654a7ca9
yomnaa3 <132593493+yomnaa3@users.noreply.github.com>
2024-05-10 16:29:38 +0300

    forget and reset paswword component

---
+++ src/Pages/ResetPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResetPassword from './../components/Login/ResetPassword';
+
+const ResetPage = () => {
+  return (
+    <div>
+      < ResetPassword/>
+    </div>
+  )
+}
+
+export default ResetPage
